4297136e9a120b719c62e769b7d1ecef43756e6e
Rafal Lewczuk <rafal.lewczuk@gmail.com>
2026-04-28 11:53:34 +0200

    Register metalium backend

--- ggml/src/ggml-backend-reg.cpp
+++ ggml/src/ggml-backend-reg.cpp
@@ -78,6 +78,10 @@
 #include "ggml-cann.h"
 #endif
 
+#ifdef GGML_USE_METALIUM
+#include "ggml-metalium.h"
+#endif
+
 #ifdef GGML_USE_ZENDNN
 #include "ggml-zendnn.h"
 #endif
@@ -157,6 +161,9 @@ struct ggml_backend_registry {
 #endif
 #ifdef GGML_USE_RPC
         register_backend(ggml_backend_rpc_reg());
+#endif
+#ifdef GGML_USE_METALIUM
+        register_backend(ggml_backend_metalium_reg());
 #endif
 #ifdef GGML_USE_OPENVINO
         register_backend(ggml_backend_openvino_reg());
@@ -565,6 +572,7 @@ void ggml_backend_load_all_from_path(const char * dir_path) {
     ggml_backend_load_best("hexagon", silent, dir_path);
     ggml_backend_load_best("musa", silent, dir_path);
     ggml_backend_load_best("openvino", silent, dir_path);
+    ggml_backend_load_best("metalium", silent, dir_path);
     ggml_backend_load_best("cpu", silent, dir_path);
     // check the environment variable GGML_BACKEND_PATH to load an out-of-tree backend
     const char * backend_path = std::getenv("GGML_BACKEND_PATH");
